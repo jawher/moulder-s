7465c848143bdf85a9a662325bec5023653698ec
Jawher Moussa <jawher.moussa@gmail.com>
2010-08-16 22:41:20 +0100

    Added tests for all available moulders

--- src/test/scala/jawher/moulder/MouldersTest.scala
+++ src/test/scala/jawher/moulder/MouldersTest.scala
@@ -45,7 +45,7 @@ object MouldersSpec extends Specification with Mockito {
       subNd must_== edc.getValue()
     }
 
-    "correctly applies its registered moulder result" in { 
+    "apply its registered moulder result" in { 
       assertXMLEqual(new StringReader("<body><outer a='v'><b>text</b>text</outer></body>"), new StringReader(
 	html(processed)))
     }
@@ -62,7 +62,7 @@ object MouldersSpec extends Specification with Mockito {
     val content = mock[Value[List[Node]]]
     content.apply() returns Some(parse("<e a='v'>c</e>text"))
 
-    val a = new Appender(content)
+    val a = Appender(content)
 
     val processed = a.process(nd, mu)
     
@@ -70,8 +70,233 @@ object MouldersSpec extends Specification with Mockito {
       there was one(content).bind(nd) then one(content).apply()
     }
 
-    "correctly applies its registered moulder result" in { 
+    "append its content after its processed element" in { 
       assertXMLEqual(new StringReader("<body><outer>test</outer><e a='v'>c</e>text</body>"), new StringReader(
+	html(processed)))
+    }
+  }
+
+  "Prepender" should {
+    XMLUnit.setIgnoreWhitespace(true);
+    val document = Jsoup.parseBodyFragment("<html><body><outer>test</outer></body></html>")
+    val mu = new MoulderUtils(document)
+
+    val element = document.getElementsByTag("outer").first()
+    val nd = (element, Some("data"))
+
+    val content = mock[Value[List[Node]]]
+    content.apply() returns Some(parse("<e a='v'>c</e>text"))
+
+    val a = Prepender(content)
+
+    val processed = a.process(nd, mu)
+    
+    "call bind then apply on its value" in { 
+      there was one(content).bind(nd) then one(content).apply()
+    }
+
+    "prepend its content before its processed element" in { 
+      assertXMLEqual(new StringReader("<body><e a='v'>c</e>text<outer>test</outer></body>"), new StringReader(
+	html(processed)))
+    }
+  }
+
+  "ChildAppender" should {
+    XMLUnit.setIgnoreWhitespace(true);
+    val document = Jsoup.parseBodyFragment("<html><body><outer>test<b a='v'>t</b>s</outer></body></html>")
+    val mu = new MoulderUtils(document)
+
+    val element = document.getElementsByTag("outer").first()
+    val nd = (element, Some("data"))
+
+    val content = mock[Value[List[Node]]]
+    content.apply() returns Some(parse("<e a='v'>c</e>text"))
+
+    val a = ChildAppender(content)
+
+    val processed = a.process(nd, mu)
+    
+    "call bind then apply on its value" in { 
+      there was one(content).bind(nd) then one(content).apply()
+    }
+
+    "append its content to its parent's children" in { 
+      assertXMLEqual(new StringReader("<body><outer>test<b a='v'>t</b>s<e a='v'>c</e>text</outer></body>"), new StringReader(
+	html(processed)))
+    }
+  }
+
+  "ChildPrepender" should {
+    XMLUnit.setIgnoreWhitespace(true);
+    val document = Jsoup.parseBodyFragment("<html><body><outer>test<b a='v'>t</b>s</outer></body></html>")
+    val mu = new MoulderUtils(document)
+
+    val element = document.getElementsByTag("outer").first()
+    val nd = (element, Some("data"))
+
+    val content = mock[Value[List[Node]]]
+    content.apply() returns Some(parse("<e a='v'>c</e>text"))
+
+    val a = ChildPrepender(content)
+
+    val processed = a.process(nd, mu)
+    
+    "call bind then apply on its value" in { 
+      there was one(content).bind(nd) then one(content).apply()
+    }
+
+    "prepend its content to its parent's children" in { 
+      assertXMLEqual(new StringReader("<body><outer><e a='v'>c</e>texttest<b a='v'>t</b>s</outer></body>"), new StringReader(
+	html(processed)))
+    }
+  }
+
+  "Remover" should {
+    XMLUnit.setIgnoreWhitespace(true);
+    val document = Jsoup.parseBodyFragment("<html><body><outer>test<b a='v'>t</b>s</outer></body></html>")
+    val mu = new MoulderUtils(document)
+
+    val element = document.getElementsByTag("outer").first()
+    val nd = (element, Some("data"))
+
+    ", given a value that returns true, " in { 
+      val remove = mock[Value[Boolean]]
+      remove.apply() returns Some(true)
+
+      val a = Remover(remove)
+
+      val processed = a.process(nd, mu)
+      "call bind then apply on its value" in { 
+        there was one(remove).bind(nd) then one(remove).apply()
+      }
+
+      "remove its element" in { 
+        assertXMLEqual(new StringReader("<body></body>"), new StringReader(
+	  html(processed)))
+      }
+    }
+
+    ", given a value that returns false, " in { 
+      val remove = mock[Value[Boolean]]
+      remove.apply() returns Some(false)
+
+      val a = Remover(remove)
+
+      val processed = a.process(nd, mu)
+      "call bind then apply on its value" in { 
+        there was one(remove).bind(nd) then one(remove).apply()
+      }
+
+      "keep its element" in { 
+        assertXMLEqual(new StringReader("<body><outer>test<b a='v'>t</b>s</outer></body>"), new StringReader(
+	  html(processed)))
+      }
+    }
+  }
+
+  "Repeater" should {
+    XMLUnit.setIgnoreWhitespace(true);
+    val document = Jsoup.parseBodyFragment("<html><body><outer a='v'>test</outer></body></html>")
+    val mu = new MoulderUtils(document)
+
+    val element = document.getElementsByTag("outer").first()
+    val nd = (element, Some("data"))
+
+    val list = 1 :: 3 :: 4 :: 7 :: Nil
+    val items = mock[Value[List[Int]]]    
+    items.apply() returns Some(list)
+
+    val a = Repeater(items)
+
+    val processed = a.process(nd, mu)
+    
+    "call bind then apply on its value" in { 
+      there was one(items).bind(nd) then one(items).apply()
+    }
+
+    "repeat its elements for every item" in { 
+      assertXMLEqual(new StringReader("<body><outer a='v'>test</outer><outer a='v'>test</outer><outer a='v'>test</outer><outer a='v'>test</outer></body>"), new StringReader(
+	html(processed)))
+    }
+
+    "associate the corresponding item to every produced element" in {
+      val it = list.iterator
+      processed.forall(it.hasNext && Some(it.next) == _._2)
+    }
+  }
+
+  "AttrModifier" should {
+    XMLUnit.setIgnoreWhitespace(true);
+    val document = Jsoup.parseBodyFragment("<html><body><outer a='v'>test</outer></body></html>")
+    val mu = new MoulderUtils(document)
+
+    val element = document.getElementsByTag("outer").first()
+    val nd = (element, Some("data"))   
+
+    ", given a value that returns something, " in { 
+      val attr = mock[Value[String]]
+      attr.apply() returns Some("b") 
+
+      val value = mock[Value[String]]
+      value.apply() returns Some("u")
+
+      val a = AttrModifier(attr, value)
+
+      val processed = a.process(nd, mu)
+      "call bind then apply on its attr and value" in { 
+        there was one(attr).bind(nd) then one(attr).apply()
+        there was one(value).bind(nd) then one(value).apply()
+      }
+
+      "add the specified attribute to its element" in { 
+        assertXMLEqual(new StringReader("<body><outer a='v' b='u'>test</outer></body>"), new StringReader(
+	  html(processed)))
+      }
+    }
+
+    ", given a value that returns nothing, " in { 
+      val attr = mock[Value[String]]
+      attr.apply() returns Some("a")
+      
+      val value = mock[Value[String]]
+      value.apply() returns None
+
+      val a = AttrModifier(attr, value)
+
+      val processed = a.process(nd, mu)
+      "call bind then apply on its attr and value" in { 
+        there was one(attr).bind(nd) then one(attr).apply()
+        there was one(value).bind(nd) then one(value).apply()
+      }
+
+      "remove the specified attribute from its element" in { 
+        assertXMLEqual(new StringReader("<body><outer>test</outer></body>"), new StringReader(
+	  html(processed)))
+      }
+    }
+  }
+
+  "ChildAppender" should {
+    XMLUnit.setIgnoreWhitespace(true);
+    val document = Jsoup.parseBodyFragment("<html><body><outer>test<b a='v'>t</b>s</outer></body></html>")
+    val mu = new MoulderUtils(document)
+
+    val element = document.getElementsByTag("outer").first()
+    val nd = (element, Some("data"))
+
+    val text = mock[Value[String]]
+    text.apply() returns Some("text")
+
+    val a = Texter(text)
+
+    val processed = a.process(nd, mu)
+    
+    "call bind then apply on its value" in { 
+      there was one(text).bind(nd) then one(text).apply()
+    }
+
+    "set its element content with its value's text" in { 
+      assertXMLEqual(new StringReader("<body><outer>text</outer></body>"), new StringReader(
 	html(processed)))
     }
   }
