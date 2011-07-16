ec986bd7b8b904b97d5624df10e5408054d595ac
Jawher Moussa <jawher.moussa@gmail.com>
2011-07-16 22:34:07 +0200

    wut ! AttrModifier

---
+++ src/main/scala/moulder/moulds/AttrModifier.scala
@@ -0,0 +1,20 @@
+package moulder.moulds
+
+import org.jsoup.nodes._
+import moulder._
+
+case class AttrModifier(private val attr: Value[String], private val value: Value[String]) extends Moulder {
+  override def process(elementAndData: (Element, Option[Any]), u: MoulderUtils): List[(Node, Option[Any])] = {
+    attr.bind(elementAndData)
+    attr() match {
+      case Some(a) => {
+        value.bind(elementAndData)
+        value() match {
+          case Some(v) => elementAndData._1.attr(a, v); List(elementAndData)
+          case None => elementAndData._1.removeAttr(a); List(elementAndData)
+        }
+      }
+      case None => List(elementAndData)
+    }
+  }
+}
