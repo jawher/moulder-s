a949c19a50a90f6fc5f230a40e55698425b367c1
Lucien Pereira <pereira.lucien@laposte.net>
2011-12-10 12:19:24 +0100

    - Because of erasure we can't be sure x is of type A.

--- src/main/scala/moulder/values/ElementDataValue.scala
+++ src/main/scala/moulder/values/ElementDataValue.scala
@@ -7,10 +7,11 @@ case class ElementDataValue[A]() extends Value[A] {
   private var value: Option[A] = None
 
   override def bind(elementAndData: (Element, Option[Any])) = {
-    value = elementAndData._2 match {
-      case v @ Some(x: A) => Some(x.asInstanceOf[A])
-      case _ => None
-    }
+    value = elementAndData._2.flatMap(x => try {
+      Some(x.asInstanceOf[A])
+    } catch {
+      case e => None
+    })
   }
 
   def apply() = value
