e09130eef9089c63a81d71edcda4dc29bdfec482
jawher <jawher.moussa@gmail.com>
2012-01-15 14:51:16 +0100

    ignore idea files

---
+++ src/main/scala/moulder/moulds/helpers/MouldersApplier.scala
@@ -0,0 +1,13 @@
+package moulder.moulds.helpers
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jawher
+ * Date: 8/1/12
+ * Time: 6:41 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+object MouldersApplier {
+
+}
